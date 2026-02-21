207504a05af156aca5d8a67cd6ded8e3558d6aa3
DiegoNavarro <diegonava1023@gmail.com>
2026-02-20 22:41:53 -0500

    Agregar redirección de /api/docs a /docs en la configuración de Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // Agregamos la redirección para que /api/docs apunte a /docs
+  async rewrites() {
+    return [
+      {
+        source: "/api/docs", // Lo que el usuario escribe en la URL
+        destination: "/docs", // La página real (docs.tsx) a la que apunta por debajo
+      },
+    ];
+  },
 };
 
 export default nextConfig;
