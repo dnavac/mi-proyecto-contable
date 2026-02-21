fcc592495f3b49b8e49cb0a4d3c8ba3d6f9d052c
DiegoNavarro <diegonava1023@gmail.com>
2026-02-20 19:52:56 -0500

    Actualizar componente App para envolver el contenido en el Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "@/components/ui/sonner";
+import Layout from "@/components/ui/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <Component {...pageProps} />
       <Toaster position="top-right" richColors />
-    </>
+    </Layout>
   );
 }
